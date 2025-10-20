3c14c4f0fb992004a000a47c0a87866a78eaf681
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-10-20 14:57:42 +0100

    add warning emoji/symbol before "warning"

--- script.mjs
+++ script.mjs
@@ -36,11 +36,11 @@ window.onload = function() {
         //Create logic to create previous misspelling warnings, and display a new message based on the number of misspelled words
         errorMessage.textContent = "";
         if (misspelledWords.length === 1) {
-            errorMessage.textContent = `Warning: The following ${misspelledWords.length} word is potentially misspelled: ${misspelledWords.toString()}`;
+            errorMessage.textContent = `⚠️ Warning: The following ${misspelledWords.length} word is potentially misspelled: ${misspelledWords.toString()}`;
         } else if (misspelledWords.length > 1) {
             const lastWord = misspelledWords.pop();
             const allWordsButLast = misspelledWords.join(", ")
-            errorMessage.textContent = `Warning: The following ${misspelledWords.length + 1} words are potentially misspelled: ${allWordsButLast} and ${lastWord}`
+            errorMessage.textContent = `⚠️ Warning: The following ${misspelledWords.length + 1} words are potentially misspelled: ${allWordsButLast} and ${lastWord}`
         } else {
             errorMessage.textContent = "";
         }
