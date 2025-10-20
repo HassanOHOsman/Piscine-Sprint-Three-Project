9c3d8f17968c7885e4b5126f9d5842588a99d574
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-10-20 14:28:22 +0100

    warning message updated and enhanced for multiple misspelled words

--- script.mjs
+++ script.mjs
@@ -33,14 +33,16 @@ window.onload = function() {
         const arrayOfWords = typedSentence.split(" ");
         const misspelledWords = wordsChecker(arrayOfWords);
 
-        console.log(misspelledWords)
-
-
+        //Create logic to create previous misspelling warnings, and display a new message based on the number of misspelled words
         errorMessage.textContent = "";
         if (misspelledWords.length === 1) {
             errorMessage.textContent = `Warning: The following ${misspelledWords.length} word is potentially misspelled: ${misspelledWords.toString()}`;
+        } else if (misspelledWords.length > 1) {
+            const lastWord = misspelledWords.pop();
+            const allWordsButLast = misspelledWords.join(", ")
+            errorMessage.textContent = `Warning: The following ${misspelledWords.length + 1} words are potentially misspelled: ${allWordsButLast} and ${lastWord}`
         } else {
-            errorMessage.textContent = misspelledWords.join(", ");
+            errorMessage.textContent = "";
         }
         document.body.append(errorMessage);
         
