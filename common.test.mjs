0c8e8b30efaf22068f5d9286bdd55911dfdc588a
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-10-22 21:20:49 +0100

    get rid of demo test

--- common.test.mjs
+++ common.test.mjs
@@ -1,12 +1,9 @@
-// import { getDictionarySize } from "./common.mjs";
+
 
 import {wordsChecker} from "./common.mjs"
 import assert, { deepStrictEqual } from "node:assert";
 import test from "node:test";
 
-// test("Dictionary size is correct", () => {
-//   assert.equal(getDictionarySize(), 856);
-// });
 
 test("Return ['shark', 'whale', 'dolphin'] since none exist in the dictionary", () => {
   assert.deepStrictEqual(wordsChecker(["shark", "whale", "dolphin"]), ["shark", "whale", "dolphin"]);
