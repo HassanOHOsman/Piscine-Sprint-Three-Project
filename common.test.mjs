f3a748183010e03f424cb31cd7bd9aea8c9fe561
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-10-22 20:49:27 +0100

    test for when part of the words exist in the dictionary

--- common.test.mjs
+++ common.test.mjs
@@ -1,16 +1,22 @@
 // import { getDictionarySize } from "./common.mjs";
 
 import {wordsChecker} from "./common.mjs"
-import assert from "node:assert";
+import assert, { deepStrictEqual } from "node:assert";
 import test from "node:test";
 
 // test("Dictionary size is correct", () => {
 //   assert.equal(getDictionarySize(), 856);
 // });
 
-test("Return array of all words when none exist in the dictionary", () => {
+test("Return ['shark', 'whale', 'dolphin'] since none exist in the dictionary", () => {
   assert.deepStrictEqual(wordsChecker(["shark", "whale", "dolphin"]), ["shark", "whale", "dolphin"]);
 });
+
+
+test("Return ['soil', 'compost'] only since they don't exist in the dictionary ", () => {
+  assert.deepStrictEqual(wordsChecker(["plant", "seed", "soil", "compost"]), ["soil", "compost"]);
+});
+
 
 
 
