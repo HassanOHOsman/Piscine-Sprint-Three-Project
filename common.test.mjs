6b4ee8d7eaab4fb5bc3b54177009d9faeb050d05
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-10-22 20:39:53 +0100

    implement test for when one of the words exist in the dictionary

--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,17 @@
-import { getDictionarySize } from "./common.mjs";
+// import { getDictionarySize } from "./common.mjs";
+
+import {wordsChecker} from "./common.mjs"
 import assert from "node:assert";
 import test from "node:test";
 
-test("Dictionary size is correct", () => {
-  assert.equal(getDictionarySize(), 856);
+// test("Dictionary size is correct", () => {
+//   assert.equal(getDictionarySize(), 856);
+// });
+
+test("Return array of all words when none exist in the dictionary", () => {
+  assert.deepStrictEqual(wordsChecker(["shark", "whale", "dolphin"]), ["shark", "whale", "dolphin"]);
 });
+
+
+
+
